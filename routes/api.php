7250f5da2018cedbb016e67043b3d7fb5d5a4549
chanhduypq <chanhduypq@gmail.com>
2020-04-23 23:57:58 +0700

    no message

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,1 @@
 <?php 
-
-use App\Http\Resources\CategoryCollection;
-use App\Models\Category;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::get('/categories', function () {
-    return new CategoryCollection(Category::paginate(2));
-});
